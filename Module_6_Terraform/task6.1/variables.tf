339076b1089d279eee443c007454605487301fac
Serhii Mykhailiuk <reorks9@gmail.com>
2020-04-14 17:54:54 +0300

    files was replaced

--- Module_6_Terraform/task6.1/variables.tf
+++ Module_6_Terraform/task6.1/variables.tf
@@ -6,10 +6,16 @@ variable "key_name" {
 }
 
 variable "aws_region" {
-  description = "AWS region to launch servers."
   default     = "us-west-2"
 }
-# variable "aws_amis" {
-#   default = {
-#     us-west-2 = "ami-0d1cd67c26f5fca19"
-#   }
+# variable "aws_ami" {
+#   default = "ami-0d1cd67c26f5fca19"
+# }
+variable "aws_ami" {
+  default = {
+    eu-west-1 = "ami-674cbc1e"
+    us-east-1 = "ami-1d4e7a66"
+    us-west-1 = "ami-969ab1f6"
+    us-west-2 = "ami-0d1cd67c26f5fca19"
+  }
+}
